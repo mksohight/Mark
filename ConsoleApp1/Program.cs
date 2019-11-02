aab1dd6067a74cc4bfc7c65a3f19ca8336ee3310
mksohight <mksohight@gmail.com>
2019-11-02 16:33:13 +0300

    first project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, lol = 0;
+            for (int x = -1; x <= 3; x++)
+            {
+                sum += x;
+                lol++;
+            }
+            Console.WriteLine(sum / lol);
+            Console.ReadKey();
+            
+        }
+    }
+}
